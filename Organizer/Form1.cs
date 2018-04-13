04d524ce1613deed80f5656abe5b35601336408b
DenysonJ <denysonjurgen@gmail.com>
2018-04-12 22:10:40 -0300

    Add search bar
    Still does not work

--- Organizer/Form1.cs
+++ Organizer/Form1.cs
@@ -6,15 +6,41 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Organizer
 {
     public partial class Form1 : Form
     {
+        string currentPath;
+        string[] files;
+        List<FileInfo> filesInfo;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentPath = Directory.GetCurrentDirectory();
+            files = Directory.GetFiles(currentPath);
+            filesInfo = new List<FileInfo>();
+
+            foreach (string thisFile in files)
+            {
+                filesInfo.Add(null);
+                try
+                {
+                    filesInfo.Add(new FileInfo(thisFile));
+                }
+                catch (FileNotFoundException Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+                
+            }
         }
     }
 }
